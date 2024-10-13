8cb06b2f39791f285473b0b585fadb8b810bf912
rensheyu <3353334311@qq.com>
2024-10-13 21:47:43 +0800

    K: finish

--- day1/K/rsy/chk.sh
+++ day1/K/rsy/chk.sh
@@ -2,12 +2,13 @@ g++ -o gen gen.cpp -O2 -std=c++17 -static
 g++ -o brute1 brute1.cpp -O2 -std=c++17 -static
 g++ -o brute2 brute2.cpp -O2 -std=c++17 -static
 g++ -o brute3 brute3.cpp -O2 -std=c++17 -static
+g++ -o gyh ../gyh/std.cpp -O2 -std=c++17 -static
 
 for i in {1..20}; do
 	./gen > input
-	./brute2 < input > answer
-	./brute3 < input > output
-	diff output answer
+	./brute3 < input > answer
+	./gyh < input > output
+	diff output answer -w -B
 	if [ $? -eq 0 ]; then
 		echo "Accepted #{$i}."
 	else
@@ -15,4 +16,4 @@ for i in {1..20}; do
 	fi
 done
 
-rm input output answer gen brute1 brute2 brute3
+rm input output answer gen brute1 brute2 brute3 gyh
