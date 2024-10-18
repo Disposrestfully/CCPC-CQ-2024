4ba06d1a9a007094664aae11b7df102c272f54f9
127command <ruinian127@gmail.com>
2024-10-18 10:47:06 +0800

    H update

---
+++ day1/H/data/run.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<sstream>
+#include<chrono>
+#include<cstdio>
+#include<windows.h>
+using namespace std;
+int main(){
+	for(int i=1;i<=20;i++)
+	{
+		string str;
+		stringstream tmp;tmp << i;tmp >> str;
+		auto start = std::chrono::high_resolution_clock::now();
+		cout<<("std.exe<"+(str)+".in>"+str+".out").c_str()<<endl;
+		system(("std.exe<"+str+".in>"+str+".out").c_str());
+		auto end = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		printf("%d %d\n",i,duration);
+	}
+}
