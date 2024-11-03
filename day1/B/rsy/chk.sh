7c6039b8c07c1b96e3811800bf35beeed9177f16
SilkMirror <2972737475@qq.com>
2024-11-03 15:57:29 +0800

    B fix data

--- day1/B/rsy/chk.sh
+++ day1/B/rsy/chk.sh
@@ -1,25 +1,25 @@
-g++ -o gen gen.cpp -O2 -std=c++17 -static
-g++ -o brute brute.cpp -O2 -std=c++17 -static
-g++ -o test test.cpp -O2 -std=c++17 -static
-g++ -o std ../psj/std.cpp -O2 -std=c++17 -static
+g++ -o gen gen.cpp -O2 -std=c++14 -static
+g++ -o brute brute.cpp -O2 -std=c++14 -static
+g++ -o test test.cpp -O2 -std=c++14 -static
+g++ -o std ../psj/std.cpp -O2 -std=c++14 -static
 
 for i in {1..100}; do
 	./gen > input
 	./brute < input > answer
 	./test < input > output
-	# ./std < input > std_output
+	./std < input > std_output
 	diff output answer -w -B
 	if [ $? -eq 0 ]; then
 		echo "Accepted #$i."
 	else
 		echo "Test code: Wrong answer!"
 	fi
-	# diff std_output answer -w -B
-	# if [ $? -eq 0 ]; then
-	# 	echo "Accepted #$i."
-	# else
-	# 	echo "Std: Wrong answer!"
-	# fi
+	diff std_output answer -w -B
+	if [ $? -eq 0 ]; then
+		echo "Accepted #$i."
+	else
+		echo "Std: Wrong answer!"
+	fi
 done
 
 rm gen brute test std input output answer std_output
--- day1/B/rsy/gen.cpp
+++ day1/B/rsy/gen.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 signed main () {
-	std::mt19937 rnd(time(0));
+	std::mt19937 rnd(time(0) ^ getpid());
 	int n = 10, k = rnd() % 3 + 1, p = rnd() % 100 + 100;
 	std::string s;
 	for (int i = 1; i <= n; i++) s += (char)(rnd() % (126 - 33 + 1) + 33);
